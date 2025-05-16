d048ea511c7377c340b5c999606eb5fb501672f5
law-liet22 <matimoce@icloud.com>
2025-05-16 13:58:19 +0000

    Comment de la post de lampes

--- Routes/lampes.js
+++ Routes/lampes.js
@@ -43,40 +43,55 @@ router.get('/', (req, res) => {
 
 router.post('/ajouter', (req, res) => {
     const code_hexa = req.body;
-    const cl = code_hexa.code_hexa
+    const cl = code_hexa.code_hexa //Récupération du code_hexa dans le body de la requête
     console.log(`Requete post pour ajouter lampe pour code_hexa=${cl}`);
     
   
     // Requête SQL pour vérifier si une lampe avec le code existe
     const verifSql = 'SELECT * FROM Lampes WHERE code_hexa="'+ cl + '";' 
 
+    // Si pas de body "code_hexa" donné
     if(!code_hexa)
     {
         return res.status(400).send({error: "Il est nécessaire de remplir le champs code_hexa"});
     }
 
+    // Envoi de la requête SQL
     db.query(verifSql, (err, result) => {
+
+        // Prise en charge d'erreur (si err)
         if(err)
         {
             console.error("Erreur lors de la verification de la presence d'une lampe avec le code " + cl + "\n Erreur : " + err);
             return res.status(500).send({error: "Erreur serveur"});
         }
+
+        // Sinon si le résultat de la requête SQL n'est pas vide (signifie lampe existante)
         else if(result != "")
         {
             console.error("Il existe deja une lampe avec le code " + cl);
             return res.status(404).send({message: "Il existe déjà une lampe avec le code " + cl});
         }
+
+        // Sinon on peut POST
         else
         {
             console.log("Il n'y a aucune lampe avec le code " + cl);
 
+            // Requête SQL pour insérer un enregistrement dans le champ code_hexa
             const sql = 'INSERT INTO Lampes (code_hexa) VALUES (?);';
+
+            // Envoi de la requête SQL 
             db.query(sql, cl, (err, result) => {
+
+                    // Si err, erreur serveur
                     if(err)
                     {
                         console.error("Erreur lors de l'ajout de la lampe avec le code hexadecimal : " + cl + "\n Erreur: " + err);
                         res.status(500).send({error: "Erreur serveur"});
                     }
+
+                    // Sinon, confirmation d'ajout
                     else
                     {
                         res.status(201).send({message: "Lampe ajoutée avec succès !", id: result.insertId});
@@ -85,7 +100,7 @@ router.post('/ajouter', (req, res) => {
             );
         }
     });
-    retourLigne(2);
+    retourLigne(2); //Retour à la ligne 2 fois dans la console pour améliorer le confort visuel
 });
 
 router.patch('/modifier/:cl', (req, res) => {
